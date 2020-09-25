83f5630f3b6f07911240fa5ab73e4f774355f6f2
ruriknj <ruriknj@gmail.com>
2020-09-25 09:17:36 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Feed from './components/Feed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+           feed: [
+               {id: 1, username: "Rurik", curtidas: 10, comentarios: 2},
+               {id: 2, username: "Lucas", curtidas: 120, comentarios: 22},
+               {id: 3, username: "Amanda", curtidas: 30, comentarios: 12},
+               {id: 4, username: "Ricardo", curtidas: 1, comentarios: 0},
+           ]
+
+        };
+    }
+        
+    render() {
+        return(
+            <div>
+             {this.state.feed.map((item) =>{
+                return (
+                    <Feed key={item.id} username={item.username}
+                    curtidas={item.curtidas} comentarios={item.comentarios} />
+                );
+             })}
+            </div>
+        );
+    }
 }
-
 export default App;
